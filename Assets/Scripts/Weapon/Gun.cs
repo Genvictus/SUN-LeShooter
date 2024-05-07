8b81afe264045677572978b3063fa6f9fc842562
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2024-05-07 14:02:20 +0700

    fix: gun reload

--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -70,7 +70,7 @@ namespace Nightmare
         public void StartReload()
         {
             Debug.Log("Reload");
-            if (!gunData.reloading)
+            if (!gunData.reloading && gameObject.activeSelf)
                 StartCoroutine(Reload());
         }
 
