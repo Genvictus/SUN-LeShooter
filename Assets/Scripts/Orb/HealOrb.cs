ba4645fad84f2c77e0b04f3950477461c54abac9
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2024-05-09 03:15:57 +0700

    feat: attack orb

--- Assets/Scripts/Orb/HealOrb.cs
+++ Assets/Scripts/Orb/HealOrb.cs
@@ -3,25 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Nightmare {
-    public class HealOrb : MonoBehaviour
+    public class HealOrb : Orb
     {
-        public float orbLifeTime = 5f;
-        public float healPercentage = 20f;
-
-        void Start()
+        public float healPercentage = 0.2f;
+        public override void ApplyOrbEffect(Collider other)
         {
-            Destroy(gameObject, orbLifeTime);
-        }
-
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Player"))
-            {
-                PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
-                float healAmount = playerHealth.maxHealth * healPercentage / 100f;
-                playerHealth.Heal(healAmount);
-                Destroy(gameObject);
-            }
+            Debug.Log("Player got heal orb");
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            float healAmount = playerHealth.maxHealth * healPercentage;
+            playerHealth.Heal(healAmount);
         }
     }
 }
