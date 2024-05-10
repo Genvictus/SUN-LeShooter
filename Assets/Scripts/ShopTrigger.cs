a32176b9cee1025b3968dffcfd655e805797ce11
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2024-05-10 16:25:34 +0700

    feat: enter shop via keypress

--- Assets/Scripts/ShopTrigger.cs
+++ Assets/Scripts/ShopTrigger.cs
@@ -8,7 +8,7 @@ public class ShopTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,10 +17,10 @@ public class ShopTrigger : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         IShopCustomer shopCustomer = other.GetComponent<IShopCustomer>();
-        if (shopCustomer != null)
+        if (shopCustomer != null && Input.GetKey(KeyCode.E))
         {
             shopUI.Show(shopCustomer);
         }
