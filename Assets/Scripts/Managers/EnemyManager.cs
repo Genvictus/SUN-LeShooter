c3ed474de600e80eabb3aebdafb8b6d93b0ab136
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2024-05-10 09:27:35 +0700

    chore: spawnrate

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -12,7 +12,7 @@ namespace Nightmare
         private float timer;
         private int spawned = 0;
 
-        void Start ()
+        void Start()
         {
             timer = spawnTime;
             playerHealth = FindObjectOfType<PlayerHealth>();
@@ -38,25 +38,25 @@ namespace Nightmare
             {
                 Spawn();
                 spawned += 1;
-                timer = spawnTime;
+                timer = spawnTime / DifficultyManager.GetEnemySpawnRate();
             }
         }
 
-        void Spawn ()
+        void Spawn()
         {
             // If the player has no health left...
-            if(playerHealth.currentHealth <= 0f)
+            if (playerHealth.currentHealth <= 0f)
             {
                 // ... exit the function.
                 return;
             }
 
             // Find a random index between zero and one less than the number of spawn points.
-            int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
             // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-            
-            Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+            Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
         }
     }
 }
